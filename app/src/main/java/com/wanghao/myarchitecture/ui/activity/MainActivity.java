dedb0221a70576e5f61d112aea1b62ff91105e95
wanghao <wanghao90dream@gmail.com>
2015-12-08 15:01:35 +0800

    toolbar滑动隐藏效果

--- app/src/main/java/com/wanghao/myarchitecture/ui/activity/MainActivity.java
+++ app/src/main/java/com/wanghao/myarchitecture/ui/activity/MainActivity.java
@@ -4,24 +4,29 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 
 import com.wanghao.myarchitecture.R;
 import com.wanghao.myarchitecture.adapter.SlidingPagerAdapter;
 
+import butterknife.Bind;
+import butterknife.BindString;
+import butterknife.ButterKnife;
+
 public class MainActivity extends FragmentActivity {
 
-    private ViewPager viewPager;
-    private TabLayout tabLayout;
+    @Bind(R.id.app_toolbar) Toolbar toolbar;
+    @Bind(R.id.viewpager) ViewPager viewPager;
+    @Bind(R.id.sliding_tabs) TabLayout tabLayout;
+    @BindString(R.string.app_name) String app_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        viewPager = (ViewPager)findViewById(R.id.viewpager);
+        ButterKnife.bind(this);
         viewPager.setAdapter(new SlidingPagerAdapter(getSupportFragmentManager()));
-
-        tabLayout = (TabLayout)findViewById(R.id.sliding_tabs);
+        toolbar.setTitle(app_name);
         tabLayout.setupWithViewPager(viewPager);
     }
 
