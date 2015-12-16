f94780cbae5077cee21a1df3650e0d5a9c75e899
wanghao <wanghao90dream@gmail.com>
2015-12-16 13:52:34 +0800

    优化结构

---
+++ app/src/main/java/com/wanghao/myarchitecture/ui/base/BaseActivity.java
@@ -0,0 +1,39 @@
+package com.wanghao.myarchitecture.ui.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+import rx.Subscription;
+import rx.subscriptions.CompositeSubscription;
+
+/**
+ * Created by wanghao on 2015/12/14.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    private CompositeSubscription mCompositeSubscription;
+
+
+    public CompositeSubscription getCompositeSubscription() {
+        if (this.mCompositeSubscription == null) {
+            this.mCompositeSubscription = new CompositeSubscription();
+        }
+
+        return this.mCompositeSubscription;
+    }
+
+
+    public void addSubscription(Subscription s) {
+        if (this.mCompositeSubscription == null) {
+            this.mCompositeSubscription = new CompositeSubscription();
+        }
+
+        this.mCompositeSubscription.add(s);
+    }
+
+    @Override protected void onDestroy() {
+        super.onDestroy();
+        if (this.mCompositeSubscription != null) {
+            this.mCompositeSubscription.unsubscribe();
+        }
+    }
+}
